68be9508dba022033e4f16cf0be4ea086aa0248b
zmiiovskyi <zmiiovskyi.m@gmail.com>
2024-03-06 21:03:25 +0200

    update data base

--- main.py
+++ main.py
@@ -19,10 +19,8 @@ bot = Bot(token=TOKEN)
 dp = Dispatcher()
 
 
-user_id = None
 @dp.message(Command("start"))
 async def cmd_start(message: types.Message) -> None:
-    global user_id
     user_id = message.from_user.id
     conn = sqlite3.connect('users.db')
     cursor = conn.cursor()
@@ -32,39 +30,36 @@ async def cmd_start(message: types.Message) -> None:
         operations TEXT
     )""")
     conn.commit()
+    cursor.execute('INSERT OR IGNORE INTO users (user_id, cash, operations) VALUES (?, ?, ?)', (user_id, 0, ''))
+    cursor.execute(f"SELECT user_id FROM users WHERE user_id = {user_id}")
+    id_user = cursor.fetchone()
     cursor.close()
     conn.close()
     await bot.send_sticker(
         user_id,
         sticker=START_STICKER,
     )
-    user(message)
     await bot.send_message(chat_id=message.chat.id, text=START_TEXT.format(message.from_user.first_name), reply_markup=keyboard.main_kb)
-    await bot.send_message(chat_id=message.chat.id, text=(f'Ваш id:{user_id}'), reply_markup=keyboard.main_kb)
+    await bot.send_message(chat_id=message.chat.id, text=(f'Ваш id:{id_user}'), reply_markup=keyboard.main_kb)
 
 def user(message: types.Message):
     user_id = message.from_user.id
     conn = sqlite3.connect('users.db')
     cursor = conn.cursor()
     cursor.execute(f"SELECT cash FROM users WHERE user_id = {user_id}")
-    user = cursor.fetchone()
-    if user is None:
-        cursor.execute(f"INSERT INTO users (user_id, cash, operations) VALUES ({user_id}, 0, '')")
-        conn.commit()
     cursor.close()
     conn.close()
 
 @dp.message(F.text=="Баланс")
-async def balance(message: types.Message, cash) -> None:
+async def balance(message: types.Message) -> None:
+    user_id = message.from_user.id
     conn = sqlite3.connect('users.db')
     cursor = conn.cursor()
-    cursor.execute(f"SELECT * FROM users WHERE user_id = {user_id}")
-    result = cursor.fetchone()
-    if result:
-        cash = result[0]
+    cursor.execute("SELECT cash FROM users WHERE user_id = ?", (user_id,))
+    cash_user = cursor.fetchall()
     cursor.close()
     conn.close()
-    await bot.send_message(chat_id=message.chat.id, text=(f"Ваш баланс: {cash}"), reply_markup=keyboard.main_kb)
+    await bot.send_message(chat_id=message.chat.id, text=(f"Ваш баланс: {cash_user}"), reply_markup=keyboard.main_kb)
 
 
 @dp.message(F.text=="Операції")
